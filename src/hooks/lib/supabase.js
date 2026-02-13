cb0733bee7a15adb406c2e1ae65b932fc83d4a60
siaful2580mm-hub <siaful2580mm@gmail.com>
2026-02-13 10:55:32 +0600

    Delete src directory

--- src/hooks/lib/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Environment Variables থেকে URL এবং Key নেওয়া হচ্ছে
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-// ক্লায়েন্ট তৈরি
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
